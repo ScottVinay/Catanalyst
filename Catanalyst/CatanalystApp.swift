fadf1dcd10840ed1ddc3ab3e1c145cde342ffc13
Scott <scott.vinay.93@gmail.com>
2026-07-20 17:27:10 +0100

    Initial Commit

---
+++ Catanalyst/CatanalystApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatanalystApp.swift
+//  Catanalyst
+//
+//  Created by Scott Vinay on 20/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct CatanalystApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatanalystTests/CatanalystTests.swift
@@ -0,0 +1,19 @@
+//
+//  CatanalystTests.swift
+//  CatanalystTests
+//
+//  Created by Scott Vinay on 20/07/2026.
+//
+
+import Testing
+@testable import Catanalyst
+
+struct CatanalystTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
